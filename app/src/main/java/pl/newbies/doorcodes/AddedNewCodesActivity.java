276c8ed65e45bbed3550ee460114b4243f3cbfc8
Paweł Koteras <pawel_koteras@wp.pl>
2017-11-29 17:25:42 +0100

    stworzenie activity do dodawania kodów

--- app/src/main/java/pl/newbies/doorcodes/AddedNewCodesActivity.java
+++ app/src/main/java/pl/newbies/doorcodes/AddedNewCodesActivity.java
@@ -2,6 +2,7 @@ package pl.newbies.doorcodes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AddedNewCodesActivity extends AppCompatActivity {
 
@@ -12,6 +13,15 @@ public class AddedNewCodesActivity extends AppCompatActivity {
     }
 
     public void onButtonClick() {
+        finish();
+    }
+
+    public void onSaveButtonClick() {
+        EditText streetEditText = (EditText) findViewById(R.id.streetEditText);
+        String street =streetEditText.getText().toString();
+        EditText codeEditText = (EditText) findViewById(R.id.codeEditText);
+        String code =codeEditText.getText().toString();
+        // TODO zapisać zmienne street i code do bazy danych
         finish();
     }
 }
