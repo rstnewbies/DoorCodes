b80c83bcf3cd0b9b3b98ff32b5cadd3a1f83c4f6
Jakub Kanicki (Alex Denton/PCMRV6) <jakub.kanicki.24@gmail.com>
2017-12-02 19:29:18 +0100

    added recycler view

---
+++ app/src/main/java/pl/newbies/doorcodes/DoorCodeAdapter.java
@@ -0,0 +1,78 @@
+package pl.newbies.doorcodes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class DoorCodeAdapter extends RecyclerView.Adapter<DoorCodeAdapter.ViewHolder>
+{
+    private Context context;
+    private List<String> content;
+
+    public DoorCodeAdapter(Context context, List<String> content)
+    {
+        this.context = context;
+        this.content = content;
+    }
+
+    @Override
+    public DoorCodeAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.row_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position)
+    {
+        final String name = content.get(position);
+        holder.txtContent.setText(name);
+        holder.txtContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DisplayDoorCodeActivity.class);
+                intent.putExtra(MainActivity.EXTRA_DOOR_ID, position);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    public void add(int position, String item)
+    {
+        content.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public void remove(int position)
+    {
+        content.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return content.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        public TextView txtContent;
+        public View layout;
+
+        public ViewHolder(View view)
+        {
+            super(view);
+            layout = view;
+            txtContent = view.findViewById(R.id.contentTextView);
+        }
+    }
+
+}
--- app/src/main/java/pl/newbies/doorcodes/MainActivity.java
+++ app/src/main/java/pl/newbies/doorcodes/MainActivity.java
@@ -1,57 +1,49 @@
 package pl.newbies.doorcodes;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
     public static final String EXTRA_DOOR_ID = "pl.newbies.doorcodes.DOOR_ID";
 
-    private ListView listView;
-    private ArrayAdapter<String> adapter;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = (ListView)findViewById(R.id.list_view);
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-        listView.setAdapter(adapter);
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
 
-        String[] content = {"ul. Wałbrzyska 13/4", "ul. Długa 5/1", "ul. Krótka 6/3"};
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
-        adapter.addAll(content);
+        List<String> content = Arrays.asList("ul. Wałbrzyska 13/4", "ul. Długa 5/1", "ul. Krótka 6/3");
 
-        listView.setOnItemClickListener(new OnItemClickListener(){
-            public void onItemClick(AdapterView parent, View view, int position, long id){
-                startDoorCodeActivity(position);
-            }});
+        adapter = new DoorCodeAdapter(this, content);
+
+        recyclerView.setAdapter(adapter);
 
         initNavigationDrawer();
-    }
-
-    public void startDoorCodeActivity(int position)
-    {
-        Intent intent = new Intent(this, DisplayDoorCodeActivity.class);
-        intent.putExtra(EXTRA_DOOR_ID, position);
-        startActivity(intent);
     }
 
 //NAVIGATION DRAWER
