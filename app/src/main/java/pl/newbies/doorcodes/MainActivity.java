ed8e792f53c588d27ba52667e97fb6d622d8c011
Jakub Kanicki (Alex Denton/PCMRV6) <jakub.kanicki.24@gmail.com>
2017-12-10 15:04:24 +0100

    modified layout for editing codes

--- app/src/main/java/pl/newbies/doorcodes/MainActivity.java
+++ app/src/main/java/pl/newbies/doorcodes/MainActivity.java
@@ -3,7 +3,6 @@ package pl.newbies.doorcodes;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,7 +13,6 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import java.util.Arrays;
 import java.util.List;
@@ -55,17 +53,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-                if (!drawer.isDrawerOpen(GravityCompat.START))
-                {
-                    drawer.openDrawer(GravityCompat.START);
-                }
-            }});
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -127,9 +114,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (id == R.id.nav_add) {
             Intent intent = new Intent(this, AddNewCodeActivity.class);
             startActivity(intent);
-        } else if (id == R.id.nav_edit) {
-            //TODO switch activities
-
         } else if (id == R.id.nav_delete) {
 
         } else if (id == R.id.nav_settings) {
