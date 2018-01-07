a9da58460a200d444be0b952b9e8fcf33ed72c1f
Jakub Kanicki (Alex Denton/PCMRV6) <jakub.kanicki.24@gmail.com>
2018-01-07 15:53:21 +0100

    added new code field validation

--- app/src/main/java/pl/newbies/doorcodes/AddNewCodeActivity.java
+++ app/src/main/java/pl/newbies/doorcodes/AddNewCodeActivity.java
@@ -1,10 +1,11 @@
 package pl.newbies.doorcodes;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddNewCodeActivity extends AppCompatActivity {
 
@@ -22,15 +23,26 @@ public class AddNewCodeActivity extends AppCompatActivity {
     public void onSaveButtonClick(View view) {
 
         EditText streetEditText = (EditText) findViewById(R.id.streetEditText);
-        String streetact = streetEditText.getText().toString();
+        String street = streetEditText.getText().toString().trim();
         EditText codeEditText = (EditText) findViewById(R.id.codeEditText);
-        String codeact = codeEditText.getText().toString();
+        String code = codeEditText.getText().toString().trim();
+
+        if(street.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(), R.string.field_street_empty_msg, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if(code.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(), R.string.field_code_empty_msg, Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         DaoSession daoSession = (App.getInstance()).getDaoSession();
         GateDao gateDao = daoSession.getGateDao();
         Gate gate = new Gate();
-        gate.name = streetact;
-        gate.code = codeact;
+        gate.name = street;
+        gate.code = code;
         gateDao.save(gate);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
